f71e15e196b57c6e997287664e249a8e9c37dd1e
Oskar <oskarzborowski@gmail.com>
2023-09-24 14:06:00 +0200

    User migration + model

---
+++ app/Enums/PersonalAccessTokenEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class PersonalAccessTokenEnum extends Enum
+{
+    const USER_AUTH = 1;
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\PersonalAccessTokenEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, HasRoles, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -30,7 +31,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -39,7 +39,17 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'id' => 'integer',
         'password' => 'hashed',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
+
+    public function createAuthToken(): string
+    {
+        $authToken = $this->createToken(PersonalAccessTokenEnum::USER_AUTH, [PersonalAccessTokenEnum::USER_AUTH]);
+        $authTokenValue = $authToken->plainTextToken;
+
+        return $authTokenValue;
+    }
 }
